2c5297e661f9b7f55682e34163b0080d1f0ab6e4
Brittany Powell <armybritbrat@gmail.com>
2023-12-29 00:37:40 -0500

    Update labchallenge06.py

--- labchallenge06.py
+++ labchallenge06.py
@@ -1,23 +1,57 @@
-# used bard for help with script and understanding the script
+# Revised
+# The Python module “os” must be utilized
+# At least three variables must be declared and referenced in Python
+# The Python function print() must be used at least three times
+# received help from A. Carroll using his script to understand
+# used bard and chatgpt to help understand the outputgi
+#\n represents the newline character
+# It's used to create line breaks within strings, instructing the program to move the text to the next line when printing or displaying it
 
-import os
+# The Python module “os” must be utilized.
 # This line imports the os module.
+import os
 
-# Declare and assign values to variables
-# Username info: os.popen("whoami") executes the whoami command using the os module.read() reads the output of the command.strip() removes leading and trailing whitespaces.
-# IP adress info: os.popen("ip a") executes the ip a command.grep '^inet ' filters the output to lines starting with "inet ".# awk '{print $2}' extracts the second word (IP address) from each line.read() reads the filtered output
-# .strip() removes leading and trailing whitespaces.
-# hardware_info:os.popen("lshw -short") executes the lshw -short command using the os module..read() reads the output of the command.
-username = os.popen("whoami").read().strip()
-ip_address = os.popen("ip a | grep '^inet ' | awk '{print $2}'").read().strip()
-hardware_info = os.popen("lshw -short").read()
 
-# Print information using the print() function
-# Prints the username with the format "Current user: username"
-# Prints the IP address with the format "IP address: IP address"
-# Prints a header "--- Hardware Information ---".
-# Prints the hardware information retrieved using os.popen.
-print(f"Current user: {username}")
-print(f"IP address: {ip_address}")
-print("--- Hardware Information ---")
-print(hardware_info)
+# Declare and initialize three variables to store bash commands
+# This command displays the current user's username
+directive1 = "whoami"  
+
+# This command displays information about network interfaces
+directive2 = "ip a"    
+
+# This command displays hardware information in a condensed format
+directive3 = "lshw -short"  
+
+# The .read() part reads the output of the command and stores it in the variable outcome1
+outcome1 = os.popen(directive1).read()
+
+
+# This line prints a descriptive header to indicate the output that will follow
+print("Result of 'whoami' command:")  
+
+# This line prints the actual output of the whoami command, which is stored in the directive1 variable.
+print(outcome1)  
+
+# Execute the second command and store the result in outcome2
+outcome2 = os.popen(directive2).read()
+
+# print("\nResult of 'ip a' command:") header
+print("\nResult of 'ip a' command:")  
+
+# Prints the output of the ip a command
+print(outcome2)  
+
+
+# Executes the third command (directive3) and stores the output in outcome3
+outcome3 = os.popen(directive3).read()
+
+
+
+# Prints a header for the output of the lshw -short command
+print("\nResult of 'lshw -short' command:") 
+
+# Prints the output of the lshw -short command for outcome3
+print(outcome3)  
+
+
+
