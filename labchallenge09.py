1191f92d77b91c6fc6b527dd803690f9b149822e
Brittany Powell <armybritbrat@gmail.com>
2024-01-03 20:43:11 -0500

    Update and rename labchallenge09 to labchallenge09.py

---
+++ labchallenge09.py
@@ -0,0 +1,58 @@
+# Basic comparisons
+
+a = 5
+b = 10
+
+# Equals
+if a == b:
+    print(f"{a} is equal to {b}")
+else:
+    print(f"{a} is not equal to {b}")
+
+# Not equals
+if a != b:
+    print(f"{a} is not equal to {b}")
+else:
+    print(f"{a} is equal to {b}")  # This won't be printed as the previous condition is true
+
+# Less than
+if a < b:
+    print(f"{a} is less than {b}")
+else:
+    print(f"{a} is not less than {b}")
+
+# Less than or equal to
+if a <= b:
+    print(f"{a} is less than or equal to {b}")
+else:
+    print(f"{a} is not less than or equal to {b}")
+
+# Greater than
+if a > b:
+    print(f"{a} is greater than {b}")
+else:
+    print(f"{a} is not greater than {b}")
+
+# Greater than or equal to
+if a >= b:
+    print(f"{a} is greater than or equal to {b}")
+else:
+    print(f"{a} is not greater than or equal to {b}")
+
+# Example with elif
+
+if a % 2 == 0:
+    print(f"{a} is even")
+elif a % 3 == 0:
+    print(f"{a} is divisible by 3")
+else:
+    print(f"{a} is neither even nor divisible by 3")
+
+# Example with both elif and else
+
+if a > b:
+    print(f"{a} is the largest number")
+elif a < b:
+    print(f"{b} is the largest number")
+else:
+    print(f"{a} and {b} are equal")
